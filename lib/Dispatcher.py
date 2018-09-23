5f500b0dfc83e189e8ac92e44ea807ed5b112c8b
naru <kmykirby@yahoo.co.jp>
2018-09-24 00:42:05 +0900

    各コマンドの説明文入れ、メソッド(action)関連追加

--- lib/Dispatcher.py
+++ lib/Dispatcher.py
@@ -5,11 +5,12 @@ import importlib
 
 
 class Dispatcher:
-    DEFAULT_ROOT_PAGE = 'index'
     DEFAULT_PROJECT_NAME = 'sample'
+    DEFAULT_PAGE = 'index'
+    DEFAULT_ACTION = 'index'
     PAGE_LIST = {
         DEFAULT_PROJECT_NAME: [
-            DEFAULT_ROOT_PAGE,
+            DEFAULT_PAGE,
             'login',
             'search',
             'admin',
@@ -19,7 +20,8 @@ class Dispatcher:
     }
 
     def __init__(self):
-        self.root_page = self.DEFAULT_ROOT_PAGE
+        self.current_page = self.DEFAULT_PAGE
+        self.current_action = self.DEFAULT_ACTION
         self.controller = None
 
     def dispatch(self, project_name=DEFAULT_PROJECT_NAME, request_uri=''):
@@ -27,12 +29,28 @@ class Dispatcher:
             return
 
         params = request_uri.split('/')
-        # 1番目のパラメーターを取得
+        # 1番目のパラメーターをコントローラー名(ページ名)として取得
         if len(params) > 0 and params[0] in self.PAGE_LIST[project_name]:
-            self.root_page = params[0]
+            self.current_page = params[0]
 
-        # capitalize:先頭の一文字を大文字、他を小文字にする
-        class_name = self.root_page.capitalize() + 'Controller'
+        # (capitalize: 先頭の一文字を大文字、他を小文字にする)
+        class_name = self.current_page.capitalize() + 'Controller'
+
+        # クラスファイル読込
         module = importlib.import_module(project_name + '.controllers.' + class_name)
+
+        # クラスインスタンス生成
         class_obj = getattr(module, class_name)
         self.controller = class_obj()
+
+        # 2番目のパラメーターをメソッド名として取得
+        if len(params) > 1:
+            self.current_action = params[1]
+
+        # (lower: 全ての文字を小文字にする)
+        action_name = self.current_action.lower() + '_action'
+
+        # アクションメソッドを実行
+        if hasattr(self.controller, action_name):
+            action_method = getattr(self.controller, action_name)
+            action_method()
