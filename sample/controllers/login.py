e977f161ac3b7790188f4fb2ac852ea4a7750372
naru <kmykirby@yahoo.co.jp>
2018-10-13 19:27:39 +0900

    ログイン画面のビュー作成

--- sample/controllers/login.py
+++ sample/controllers/login.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-from lib.bottle import Bottle, TEMPLATE_PATH
+from lib.bottle import Bottle, TEMPLATE_PATH, jinja2_view
 from sys import argv
 
 
 app = Bottle()
-TEMPLATE_PATH.append('./sample/view')
+TEMPLATE_PATH.append('./sample/views')
 
 
 @app.route('/')
-@app.jinja2_view('login.html')
+@jinja2_view('login.html')
 def index():
-    return {'message': 'This is sample_{} page.'.format(argv[0])}
+    return {
+        'message': 'This is sample_{} page.'.format(argv[0]),
+        'title': u'ログイン'
+    }
